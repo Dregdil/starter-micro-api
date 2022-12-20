a7d3dd4a84d6579c313a941411f6c0a5d37662e6
Dregdil <65603166+Dregdil@users.noreply.github.com>
2022-12-20 14:22:03 +0100

    e

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const url = require('url');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
 	const liczba1 = req.query.liczba1
-    res.write(liczba1);
+    res.send(liczba1);
     res.end();
 }).listen(process.env.PORT || 3000);
