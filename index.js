bc80cd230f19e31d118a1471f4d489fecadb769d
Dregdil <65603166+Dregdil@users.noreply.github.com>
2022-12-20 14:50:29 +0100

    rrr

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+function add(a,b) {
+  return a+b;
+}
 const app = express();
 app.get('/api',function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
 	const liczba1 = req.query.liczba1
 	const liczba2 = req.query.liczba2
-    res.send({"liczba1": liczba1});
+	var data = add(liczba1,liczba2)
+    res.send({'Wynik dodawania: ${liczba1}+${liczba2} ='data:});
 }).listen(process.env.PORT || 3000);
