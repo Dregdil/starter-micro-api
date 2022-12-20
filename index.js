be357e1fe41452b7321bbf39658c7a47c6880844
Dregdil <65603166+Dregdil@users.noreply.github.com>
2022-12-20 14:20:05 +0100

    c22

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
+const url = require('url');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write(2+2);
+	const liczba1 = req.query.liczba1
+    res.write(liczba1);
     res.end();
 }).listen(process.env.PORT || 3000);
