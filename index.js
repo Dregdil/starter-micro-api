a4f1cf4fd3c95a2b23f37272960ab523e405d31b
Dregdil <65603166+Dregdil@users.noreply.github.com>
2022-12-20 13:41:55 +0100

    test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+	const urlParams = new URLSearchParams(queryString);
+	
+    res.write(urlParams.get("liczba1"));
     res.end();
 }).listen(process.env.PORT || 3000);
