872b23a2d0f8c9d0eed231c1ac2fef3dc3b6393b
Dregdil <65603166+Dregdil@users.noreply.github.com>
2022-12-20 15:20:50 +0100

    rrr

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/api',function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
 	const liczba1 = req.query.liczba1
 	const liczba2 = req.query.liczba2
-	var data = add(liczba1,liczba2)
-    res.send({'Wynik dodawania: ${liczba1}+${liczba2} ='data:});
+	var data = add(parseInt(liczba1), parseInt(liczba2))
+    res.send({'Wynik dodawania: ${liczba1}+${liczba2} = 'data});
 }).listen(process.env.PORT || 3000);
